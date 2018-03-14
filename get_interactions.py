4690ff9e4240e7ac721b3506c3722b996b7f0f43
martaferri <35527487+martaferri@users.noreply.github.com>
2018-03-14 15:26:11 +0100

    Update get_interactions.py
    - Creation of the files.
    - Cut-off distance = 8A. (https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4894841/pdf/nihms789034.pdf)

--- get_interactions.py
+++ get_interactions.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
-
+import sys
 import Bio.PDB
 import numpy
 import itertools
+import re
+
+if len(sys.argv) <= 1:
+    print('USAGE: python3 file.pdb')
+    exit()
 
 # Opening the pdb without chains I and J because they were giving problems
-complex_struct = open('3kuy_ok.pdb', 'r')
+complex_struct = open(sys.argv[1], 'r')
 
 # Creating the object from the complex
 p = Bio.PDB.PDBParser()
-structure = p.get_structure('3kuy', complex_struct)
+structure = p.get_structure('prot', complex_struct)
 
 # Function to calculate distances between atoms, return a tuple with min, max and number of atoms
 def calc_distances_residues(chain1, chain2):
@@ -49,14 +54,32 @@ for model in structure:
 					# print("2",chain2)
 					main_dict[chain1.get_id()][chain2.get_id()] = calc_distances_residues(chain1, chain2)
 
-print(main_dict)
+# print(main_dict)
 
 # Setting a cut-off value to determine if there is or not an interaction. Storing the interactions in a list
 interactions_list = []
 for element in main_dict:
 	for key, value in main_dict[element].items():
-		if value[0] < 6: 
+		if value[0] <= 8: 
 			interactions_list.append(element + key)
-print("\n")
+
 print(interactions_list)
 
+complex_struct.close()
+
+# Creating the new pair files
+for element in interactions_list:
+	pdb = open(sys.argv[1], 'r')
+	fo = open(sys.argv[1][0:-4] + "_" + element + ".pdb", "w")
+	for line in pdb:
+		if line.startswith('ATOM'):
+			line = line.strip()
+			search = re.search(r'[A-Z]*\s*\d{1,3}\s*[A-Z]{1,2}[0-9]?\s*[A-Z]{3}\s([A-Z]).*', line)
+			if search:
+				chain = search.group(1)			
+				if chain == element[0]:
+					fo.write("%s\n"%(line))
+				elif chain == element[1]:
+					fo.write("%s\n"%(line))
+	fo.close()
+	pdb.close()		
