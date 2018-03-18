d280cc96275a80a10a4c7049e4c5c40115fbb182
martaferri <35527487+martaferri@users.noreply.github.com>
2018-03-18 12:00:37 +0100

    Add files via upload

--- utilities.py
+++ utilities.py
@@ -1,11 +1,6 @@
 import string
 
 
-# Dict to convert the pdb file into fasta
-letters = {'ALA':'A','ARG':'R','ASN':'N','ASP':'D','CYS':'C','GLU':'E','GLN':'Q','GLY':'G','HIS':'H',
-           'ILE':'I','LEU':'L','LYS':'K','MET':'M','PHE':'F','PRO':'P','SER':'S','THR':'T','TRP':'W',
-           'TYR':'Y','VAL':'V'}
-
 # List of ASCII characters to create new IDs
 ascii_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
               'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F',
@@ -14,3 +9,11 @@ ascii_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
               '[', ']', '^', '_', '`', '{', '|', '}', '~']
 
 ascii_list2 = list(string.printable)
+
+# Dict to convert the pdb file into fasta
+three_to_one = {'ALA':'A','ARG':'R','ASN':'N','ASP':'D','CYS':'C','GLU':'E','GLN':'Q','GLY':'G','HIS':'H',
+           'ILE':'I','LEU':'L','LYS':'K','MET':'M','PHE':'F','PRO':'P','SER':'S','THR':'T','TRP':'W',
+           'TYR':'Y','VAL':'V'}
+
+# Dict to convert the amino seq 
+one_to_three = {'C': 'CYS', 'D': 'ASP', 'S': 'SER', 'Q': 'GLN', 'K': 'LYS', 'I': 'ILE', 'P': 'PRO', 'T': 'THR', 'F': 'PHE', 'N': 'ASN', 'G': 'GLY', 'H': 'HIS', 'L': 'LEU', 'R': 'ARG', 'W': 'TRP', 'A': 'ALA', 'V': 'VAL', 'E': 'GLU', 'Y': 'TYR', 'M': 'MET', 'X': 'UNK'}
